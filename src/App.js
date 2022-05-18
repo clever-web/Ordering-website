52553b74a3d89a148950d616815e6dfcb275cd14
superstar <83311833+clever-web@users.noreply.github.com>
2022-05-18 15:15:04 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+// components
+import Settings from './components/settings';
+import RtlLayout from './components/RtlLayout';
+import ScrollToTop from './components/ScrollToTop';
+import ThemePrimaryColor from './components/ThemePrimaryColor';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <ThemeConfig>
+      <ThemePrimaryColor>
+        <RtlLayout>
+          <Settings />
+          <ScrollToTop />
+          <Router />
+        </RtlLayout>
+      </ThemePrimaryColor>
+    </ThemeConfig>
+  );
+}
